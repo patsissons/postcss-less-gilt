2e37cd904197ad342403f193f1cbbe45cf6dc3b4
Pat Sissons <psissons@acrotech.io>
2016-03-10 14:58:53 -0800

    adding a basic mixins tokenizer test

--- test/tokenize.spec.js
+++ test/tokenize.spec.js
@@ -19,6 +19,29 @@ describe('#tokenize()', () => {
     ]);
   });
 
+  it('tokenizes mixins', () => {
+    testTokens('.foo (@bar; @baz...) { border: @{baz}; }', [
+      ['word', '.foo', 1, 1, 1, 4],
+      ['space', ' '],
+      ['(', '(', 1, 6],
+      ['mixin-param', '@bar', 1, 7, 1, 10],
+      [';', ';', 1, 11],
+      ['space', ' '],
+      ['mixin-param', '@baz...', 1, 13, 1, 19, 'var-dict'],
+      [')', ')', 1, 20],
+      ['space', ' '],
+      ['{', '{', 1, 22],
+      ['space', ' '],
+      ['word', 'border', 1, 24, 1, 29],
+      [':', ':', 1, 30],
+      ['space', ' '],
+      ['word', '@{baz}', 1, 32, 1, 37],
+      [';', ';', 1, 38],
+      ['space', ' '],
+      ['}', '}', 1, 40]
+    ]);
+  });
+
   describe('Comments', () => {
     it('tokenizes inline comments', () => {
       testTokens('// a\n', [
